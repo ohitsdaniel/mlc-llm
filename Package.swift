b7ad3ae413072bdfdc97f2d9fec330db60be19ea
Daniel Peter <daniel.peter@me.com>
2025-11-18 12:39:10 +0100

    Adds Package.swift to the project root

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MLCSwift",
+    products: [
+        .library(
+            name: "MLCSwift",
+            targets: ["MLCEngineObjC", "MLCSwift"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MLCEngineObjC",
+            path: "ios/MLCSwift/Sources/ObjC",
+            cxxSettings: [
+                .headerSearchPath("../../../3rdparty/tvm/include"),
+                .headerSearchPath("../../../3rdparty/tvm/3rdparty/tvm-ffi/include"),
+                .headerSearchPath("../../../3rdparty/tvm/3rdparty/tvm-ffi/3rdparty/dlpack/include"),
+                .headerSearchPath("../../../3rdparty/tvm/3rdparty/dmlc-core/include")
+            ]
+        ),
+        .target(
+            name: "MLCSwift",
+            dependencies: ["MLCEngineObjC"],
+            path: "ios/MLCSwift/Sources/Swift"
+        )
+    ],
+    cxxLanguageStandard: .cxx17
+)
